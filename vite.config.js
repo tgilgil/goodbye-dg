381d3724bdc59e85486cbfd507ebce6d00dcea79
Ali Gilgil <ali.gilgil1@gmail.com>
2026-04-02 11:19:39 -0400

    ga for pages

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // If deploying to username.github.io/repo-name/, set base to '/repo-name/'
+  // If deploying to username.github.io/, set base to '/'
+  base: './', 
+})
